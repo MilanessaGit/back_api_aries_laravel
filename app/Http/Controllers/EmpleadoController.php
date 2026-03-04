d5c6b55ce4e7d090acda105f17d07a26ee97591e
MilanessaGit <miguel.aguirre.v12@gmail.com>
2026-03-04 01:57:40 -0400

    Agregacion de controladores

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Empleado;
 use Illuminate\Http\Request;
 
 class EmpleadoController extends Controller
@@ -11,9 +12,14 @@ class EmpleadoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if($request->q){
+            $empleados = Empleado::where('ci_nit' , 'like', "%" . $request->q . "%")->first();
+        }else{
+            $empleados = Empleado::paginate(5);
+        }
+        return response()->json($empleados);
     }
 
     /**
@@ -24,7 +30,30 @@ class EmpleadoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validar
+        $request->validate([
+            'nombre' => 'required|string',
+            'apellido' => 'required|string',
+            'ci_nit' => 'required|string|unique:empleados',
+            'edad' => 'required|integer',
+            'telefono' => 'nullable|string',
+            'direccion' => 'nullable|string'
+            //,'user_id' => 'required'
+        ]);
+
+        //guardar
+        $emp = new Empleado();
+        $emp->nombre = $request->nombre;
+        $emp->apellido = $request->apellido;
+        $emp->ci_nit = $request->ci_nit;
+        $emp->edad = $request->edad;
+        $emp->telefono = $request->telefono;
+        $emp->direccion = $request->direccion;
+        //$emp->user_id = $request->user_id;
+        $emp->save();
+
+        return response()->json(['message' => 'Empleado creado exitosamente'], 201);
+
     }
 
     /**
@@ -35,7 +64,12 @@ class EmpleadoController extends Controller
      */
     public function show($id)
     {
-        //
+        $empleado = Empleado::find($id);
+        if (!$empleado) {
+            return response()->json(['message' => 'Empleado no encontrado'], 404);
+        }
+
+        return response()->json($empleado, 200);
     }
 
     /**
@@ -47,7 +81,30 @@ class EmpleadoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //validar
+        $request->validate([
+            'nombre' => 'required|string',
+            'apellido' => 'required|string',
+            //'ci_nit' => 'required|string|unique:empleados,ci_nit,' . $id,
+            'edad' => 'required|integer',
+            'telefono' => 'nullable|string',
+            'direccion' => 'nullable|string'
+            //,'user_id' => 'required'
+        ]);
+
+        //guardar
+
+        $emp = Empleado::find($id);
+        $emp->nombre = $request->nombre;
+        $emp->apellido = $request->apellido;
+        $emp->ci_nit = $request->ci_nit;
+        $emp->edad = $request->edad;
+        $emp->telefono = $request->telefono;
+        $emp->direccion = $request->direccion;
+        //$emp->user_id = $request->user_id;
+        $emp->update();
+
+        return response()->json(['message' => 'Empleado Modificado Exitosamente'], 200);
     }
 
     /**
@@ -58,6 +115,9 @@ class EmpleadoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $emp = Empleado::find($id);
+        $emp->delete();
+
+        return response()->json(['message' => 'Empleado Eliminado Exitosamente'], 200);
     }
 }
--- app/Http/Controllers/SalidaController.php
+++ app/Http/Controllers/SalidaController.php
@@ -11,9 +11,24 @@ class SalidaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // /api/salida?page=2&limit=10$q=1223&orderby=id
+        //$page = $request->page; // ya captura por si solo sin guardarlo en una variable
+        $limit = $request->limit?$request->limit:10;
+        $q = $request->q; //valor de busqueda
+        $orderby = $request->orderby?$request->orderby:'id';
+
+        if($q){
+            $salidas = Salida::where('codigo_salida','like',"%$q%")
+            ->orderBy($orderby,'desc')
+            ->paginate($limit);
+
+        }else{
+            $salidas = Salida::orderBy($orderby,'desc')->paginate($limit);
+        }
+
+        return response()->json($salidas, 200);
     }
 
     /**
@@ -24,7 +39,30 @@ class SalidaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validar
+        $request->validate([
+            'codigo_salida' => 'required|unique:salidas',
+            'fecha' => 'required|date',
+            'total' => 'nullable|numeric',
+            'tipo' => 'nullable|in:venta,consumo',
+            'observaciones' => 'nullable|string',
+            //'cantidad' => 'required|numeric',
+            'empleado_id' => 'required' //|exists:empleados,id'
+        ]);
+
+        // guardar
+        $salida = new Salida();
+        $salida->codigo_salida = $request->codigo_salida;
+        $salida->fecha = $request->fecha;
+        $salida->total = $request->total;
+        $salida->tipo = $request->tipo;
+        $salida->observaciones = $request->observaciones;
+        $salida->empleado_id = $request->empleado_id;
+        $salida->save();
+
+        return response()->json(['message' => 'Salida registrada exitosamente', 'data' => $salida]);
+
+        
     }
 
     /**
