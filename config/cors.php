11d1038c653ee84b64e567725670db6951007079
MilanessaGit <miguel.aguirre.v12@gmail.com>
2026-08-14 20:38:39 -0400

    Corregir CORS para rutas v1

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'v1/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
